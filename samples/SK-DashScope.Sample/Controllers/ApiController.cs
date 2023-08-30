511361e20d8d0697faae1fdf5ab27eade5acadee
zhengdi <zhengdi@custouch.com>
2023-08-30 16:52:22 +0800

    Add KernelBuilder and EmbeddingGeneration

---
+++ samples/SK-DashScope.Sample/Controllers/ApiController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.TextCompletion;
+using SK_DashScope.Sample.Models;
+using System.Text;
+
+namespace SK_DashScope.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly IKernel kernel;
+
+        public ApiController(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChatAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                return NoContent();
+            }
+
+            var chat = kernel.GetService<IChatCompletion>();
+            var history = chat.CreateNewChat();
+            history.AddUserMessage(input.Text);
+
+            var result = await chat.GenerateMessageAsync(history);
+            return Ok(result);
+        }
+
+        [HttpPost("text")]
+        public async Task<IActionResult> TextAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                return NoContent();
+            }
+
+            var completion = kernel.GetService<ITextCompletion>();
+            var result = await completion.GetCompletionsAsync(input.Text, new CompleteRequestSettings { TopP = 0.8 }, cancellationToken);
+            var text = await result.First().GetCompletionAsync();
+            return Ok(text);
+        }
+
+        [HttpPost("stream")]
+        public async Task ChatStreamAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                await Response.CompleteAsync();
+            }
+
+            var chat = kernel.GetService<IChatCompletion>();
+            var history = chat.CreateNewChat();
+            history.AddUserMessage(input.Text);
+
+            var results = chat.GenerateMessageStreamAsync(history, cancellationToken: cancellationToken);
+
+            await foreach (var result in results)
+            {
+                await Response.WriteAsync("data: " + result + "\n\n", Encoding.UTF8);
+                await Response.Body.FlushAsync();
+            }
+
+            await Response.CompleteAsync();
+        }
+
+        [HttpPost("embedding")]
+        public async Task<IActionResult> EmbeddingAsync([FromBody] UserInput input, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(input.Text))
+            {
+                return NoContent();
+            }
+
+            var embedding = kernel.GetService<ITextEmbeddingGeneration>();
+
+            ReadOnlyMemory<float> result = await embedding.GenerateEmbeddingAsync(input.Text, cancellationToken);
+
+            var serializableList = result.ToArray().ToList();
+
+            return Ok(serializableList);
+        }
+    }
+}
